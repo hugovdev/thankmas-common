58a7a7b427d8a04deadd62ded96d4037b85bcc7e
Hugo Vallejo <hugo.vallejo@noxcrew.com>
2023-12-16 15:03:04 +0100

    Add cooldown system to PlayerData.

--- src/main/kotlin/me/hugo/thankmas/player/PlayerData.kt
+++ src/main/kotlin/me/hugo/thankmas/player/PlayerData.kt
@@ -1,8 +1,31 @@
 package me.hugo.thankmas.player
 
-import java.util.UUID
+import java.util.*
+import kotlin.time.Duration
 
-/**
- * Base player data class.
- */
-public open class PlayerData(protected val playerUUID: UUID)
+/** Base player data class with a simple cooldown system. */
+public open class PlayerData(protected val playerUUID: UUID) {
+
+    /** Map that contains the timestamp of when each cooldown ends. */
+    private val activeCooldowns: MutableMap<String, Long> = mutableMapOf()
+
+    /** @returns whether a player's cooldown of [id] is active. */
+    public fun isOnCooldown(id: String): Boolean {
+        val finishTime: Long = activeCooldowns[id] ?: return false
+
+        return finishTime > System.currentTimeMillis()
+    }
+
+    /** @returns the remaining waiting time for the cooldown with id [id]. */
+    public fun getRemainingCooldown(id: String): Int {
+        val finishTime: Long = activeCooldowns[id] ?: return 0
+
+        return (finishTime - System.currentTimeMillis()).toInt() / 1000
+    }
+
+    /** Sets the waiting time for the cooldown with id [id] to [time]. */
+    public fun setCooldown(id: String, time: Duration) {
+        activeCooldowns[id] = System.currentTimeMillis() + time.inWholeMilliseconds
+    }
+
+}
