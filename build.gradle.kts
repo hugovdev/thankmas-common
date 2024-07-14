05e9a46a420d380d588eec033101bf491abcf590
Hugo Vallejo <hugo.vallejo@noxcrew.com>
2024-07-14 16:23:44 +0200

    Thankmas monorepo!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,22 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("com.google.devtools.ksp") version "1.9.21-1.0.15"
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow")
+    id("com.google.devtools.ksp")
 }
 
 group = "me.hugo.thankmas"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-
-    maven {
-        name = "papermc-repo"
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
-
-    maven { url = uri("https://nexus.leonardbausenwein.de/repository/maven-public/") }
-}
-
 dependencies {
     compileOnly("net.kyori:adventure-api:4.17.0")
     compileOnly("net.kyori:adventure-text-minimessage:4.16.0")
 
-    implementation("io.insert-koin:koin-core:3.5.6")
-    implementation("io.insert-koin:koin-annotations:1.3.1")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-    ksp("io.insert-koin:koin-ksp-compiler:1.3.1")
+    api(libs.bundles.koin)
+    api(libs.hikariCP)
 
-    implementation(files("libs/miniphrase-core-1.0.0-SNAPSHOT.jar"))
+    ksp(libs.koin.ksp.compiler)
+
+    api(files("C:/Users/hugov/IdeaProjects/thankmas/common/libs/miniphrase-core-1.0.0-SNAPSHOT.jar"))
 }
 
 tasks.test {
